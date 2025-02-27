6bc4685fdcb36c22904c912cab2a9d142862b91b
mohammadrizvy <rizvyhq1@gmail.com>
2025-02-27 15:27:01 +0600

    feat: add request validation middleware for academic semester creation endpoint

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,10 +1,18 @@
 import express from 'express';
 import { academicSemesterController } from './academicSemester.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { academicSemesterValidation } from './academicSemester.validation';
 
 const router = express.Router();
 
 // This will call controller function
-router.post('/create-academic-semester', academicSemesterController.createAcademicSemester)
+router.post(
+  '/create-academic-semester',
+  validateRequest(
+    academicSemesterValidation.createAcademicSemesterValidationSchema,
+  ),
+  academicSemesterController.createAcademicSemester,
+);
 // router.get('/students', studentControllers.getStudents);
 // router.get('/:studentId', studentControllers.getSingleStudent);
 // router.delete('/:studentId', studentControllers.DeleteStudent);
