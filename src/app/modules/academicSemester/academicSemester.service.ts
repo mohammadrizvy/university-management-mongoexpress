cdda3582a55f0e6c3e1758ba3f9c5c47f4fec015
mohammadrizvy <rizvyhq1@gmail.com>
2025-03-04 13:29:36 +0600

    feat: add validation for academic semester code based on name mapping

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -4,15 +4,19 @@ import { AcademicSemester } from './academicSemester.model';
 const createAcademicSemesterIntoDB = async (payload: TacademicSemester) => {
   //* MATCHING (Semester name ---> Semester code)
 
-  type TacademicSemesterNameCodeMapper ={
-    [Key : string] : string
-  }
+  type TacademicSemesterNameCodeMapper = {
+    [Key: string]: string;
+  };
 
-  const academicSemesterNameCodeMapper : TacademicSemesterNameCodeMapper = {
+  const academicSemesterNameCodeMapper: TacademicSemesterNameCodeMapper = {
     Autum: '01',
     Summer: '02',
     Fall: '03',
   };
+
+  if(academicSemesterNameCodeMapper[payload.name] !== payload.code){
+    throw new Error ("Invalid Semester Code")
+  }
 
   const reslut = await AcademicSemester.create(payload);
   return reslut;
