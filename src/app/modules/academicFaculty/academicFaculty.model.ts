a1b56843bbd466943f71b062f3ef2f79ea186470
MD Ehsanul Haque Rizvy <rizvyhq1@gmail.com>
2025-03-07 23:49:14 +0600

    feat: refine academic faculty model schema and add validation schema using Zod

--- src/app/modules/academicFaculty/academicFaculty.model.ts
+++ src/app/modules/academicFaculty/academicFaculty.model.ts
@@ -1,13 +1,11 @@
 import { model, Schema } from 'mongoose';
-import config from '../../config';
-import bcrypt from 'bcrypt';
 import { TAcademicFaculty } from './academicFaculty.interface';
 
 const academicFacultySchema = new Schema<TAcademicFaculty>(
-  {
-   name : {type : String , required : true , unique : true }
-  },
-  { timestamps: true },
+    {
+        name: { type: String, required: true, unique: true }
+    },
+    { timestamps: true },
 );
 
 
