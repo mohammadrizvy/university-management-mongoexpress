a9ce72ad91972200b15752ec77a797ceb21ac2f3
mohammadrivy <rizvyhq1@gmail.com>
2025-03-31 03:03:32 +0600

    feat: add route for creating faculty with validation

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { userController } from './user.controller';
 import { studentValidations } from '../Students/student.validation';
 import validateRequest from '../../middlewares/validateRequest';
+import { facultyValidations } from '../Faculty/faculty.validation';
 
 const router = express.Router();
 
@@ -9,6 +10,10 @@ router.post(
   '/create-students',
   validateRequest(studentValidations.createSudentValidatedSchema),
   userController.createStudent,
+);
+router.post(
+  '/create-faculty',
+  validateRequest(facultyValidations.createFacultyValidationSchema),
 );
 
 export const userRoutes = router;
