7e242ad0f7402b32fe6d0626502666f1abc1d062
mohammadrivy <rizvyhq1@gmail.com>
2025-03-31 02:31:00 +0600

    feat: define Faculty model with detailed schema and nested name structure

--- src/app/modules/Faculty/faculty.model.ts
+++ src/app/modules/Faculty/faculty.model.ts
@@ -1,3 +1,56 @@
-import { Schema } from "mongoose";
+import { model, Schema } from 'mongoose';
+import { TFaculty, TFacultyName } from './faculty.interface';
 
-const facultySchema = new Schema(Tfactu)
+const facultyNameSchema = new Schema<TFacultyName>({
+  firstName: {
+    type: String,
+  },
+  secoundName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+});
+
+const facultySchema = new Schema<TFaculty>(
+  {
+    id: { type: String, unique: true },
+    user: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      query: true,
+      ref: 'User',
+    },
+    name: facultyNameSchema,
+    gender: {
+      type: String,
+      enum: ['male', 'female'],
+    },
+    DOB: { type: Date },
+    contactNo: { type: Number },
+    emergencyContact: { type: Number },
+    email: { type: String },
+    presentAddress: {
+      type: String,
+    },
+    profileImage: {
+      type: String,
+    },
+    academicDepartment: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicSemester',
+    },
+    academicFaculty: { type: Schema.Types.ObjectId, ref: 'AcademicFaculty' },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+
+export const Faculty = model<TFaculty>("Faculty" , facultySchema)
