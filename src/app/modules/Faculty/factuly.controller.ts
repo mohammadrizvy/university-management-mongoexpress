201a85262019651323c04f85433c039c222b7553
mohammadrivy <rizvyhq1@gmail.com>
2025-04-05 01:40:30 +0600

    feat: implement response handling for retrieving a single faculty and fix response structure

--- src/app/modules/Faculty/factuly.controller.ts
+++ src/app/modules/Faculty/factuly.controller.ts
@@ -1,7 +1,7 @@
 import catchAsync from "../../utils/catchAsync";
-import sendResponse from "../../utils/sendResponse";
 import { FacultyServices } from "./faculty.service";
 import httpStatus from 'http-status';
+import sendResponse from './../../utils/sendResponse';
 
 const getFaculty = catchAsync(async (req, res, next) => {
     const result = await FacultyServices.getFacultyFromDB()
@@ -16,9 +16,14 @@ const getFaculty = catchAsync(async (req, res, next) => {
 const getSingleFaculty = catchAsync(async (req, res, next) => {
     const factultyId = req.params.facultyId
     console.log(factultyId, "From controller")
-    const result = await FacultyServices.getSingleFacultyFromDB(factultyId),
-    send
-
+    const result = await FacultyServices.getSingleFacultyFromDB(factultyId)
+    sendResponse(res , {
+        sucess : true ,
+        statusCode : httpStatus.FOUND,
+        message : "Single faculty retrived sucessfully",
+        data : result
+    })
+    
 })
 
 
