fdd7cae2e5906f6ebba68d89d490e37c86a7feee
mohammadrizvy <rizvyhq1@gmail.com>
2025-05-16 03:01:07 +0600

    feat(semester-registration): enhance createSemesterRegistrationIntoDB with validation checks for existing semesters

--- src/app/modules/semesterRegistration/semisterRegistration.controller.ts
+++ src/app/modules/semesterRegistration/semisterRegistration.controller.ts
@@ -3,13 +3,15 @@ import sendResponse from '../../utils/sendResponse';
 import httpStatus from 'http-status';
 import { semesterRegistrationServices } from './semisterRegistration.service';
 
-const createSemesterRegistration = catchAsync(async (req, res ) => {
+const createSemesterRegistration = catchAsync(async (req, res) => {
   const result =
-    await semesterRegistrationServices.createSemesterRegistrationIntoDB(req.body);
+    await semesterRegistrationServices.createSemesterRegistrationIntoDB(
+      req.body,
+    );
 
   sendResponse(res, {
     sucess: true,
-    statusCode: httpStatus.FOUND,
+    statusCode: httpStatus.CREATED,
     message: 'Semster Registerd succesfully',
     data: result,
   });
--- src/app/modules/semesterRegistration/semisterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semisterRegistration.service.ts
@@ -1,9 +1,45 @@
-import { TSemesteRegistration } from "./semisterRegistration.interface";
+import { AppError } from '../../Errors/AppErrors';
+import { AcademicSemester } from '../academicSemester/academicSemester.model';
+import { TSemesteRegistration } from './semisterRegistration.interface';
+import httpStatus from 'http-status';
+import { SemesterRegistration } from './semisterRegistration.model';
 
-const createSemesterRegistrationIntoDB = async (payload : TSemesteRegistration) => {
+const createSemesterRegistrationIntoDB = async (
+  payload: TSemesteRegistration,
+) => {
+  /**
+   * (1) So basically what happend here is , First we check if the the semester dose exits on DB or not .
+   * If not then it will show thorw and an error .
+   * (2) Then in 2nd phase we are checking if the semester is already registerd or not .
+   * If not then it will pass or if the semster is alredy registerd it will throw conflict error that "This semester is already registerd!"
+   */
 
+  const academicSemester = payload?.academicSemester;
 
+  // | Check if the semester dose exits?
+  const isAcademicSemesterExits =
+    await AcademicSemester.findById(academicSemester);
+  if (!isAcademicSemesterExits) {
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      'This academic semester is not found !',
+    );
+  }
+  // ||  Check if the semester is already registerd or not
+  const isSemesterRegistrationExits = await SemesterRegistration.findOne({
+    academicSemester,
+  });
 
+  if (isSemesterRegistrationExits) {
+    throw new AppError(
+      httpStatus.CONFLICT,
+      'This semester is already registerd ! ',
+    );
+  }
+
+  const result = await SemesterRegistration.create(payload);
+
+  return result;
 };
 
 const getAllSemesterRegistrationFromDB = async () => {};
