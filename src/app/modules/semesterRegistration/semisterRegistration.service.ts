1aa86042f1381d0e44deb890c67093c456e4ed8e
mohammadrizvy <rizvyhq1@gmail.com>
2025-05-16 01:06:29 +0600

    feat(semester-registration): implement initial service functions for semester registration

---
+++ src/app/modules/semesterRegistration/semisterRegistration.service.ts
@@ -0,0 +1,19 @@
+const createSemesterRegistrationIntoDB = async () => {
+
+    
+
+}
+
+const getAllSemesterRegistrationFromDB = async () => {}
+
+const getSingleSemesterRegistrationFromDB = async () =>{}
+
+const updateSemesterRegistrationIntoDB = async () => {}
+
+
+export const semesterRegistrationServices = {
+    createSemesterRegistrationIntoDB,
+    getAllSemesterRegistrationFromDB,
+    getSingleSemesterRegistrationFromDB,
+    updateSemesterRegistrationIntoDB,
+}
