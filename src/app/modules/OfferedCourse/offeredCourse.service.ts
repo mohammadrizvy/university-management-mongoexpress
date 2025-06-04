41fcd65e4bf91c0328e22b5d62e8bee80fed62f1
mohammadrivy <rizvyhq1@gmail.com>
2025-06-05 01:57:17 +0600

    refactor(OfferedCourse): improve code readability by restructuring variable declarations and enhancing error messages

--- src/app/modules/OfferedCourse/offeredCourse.service.ts
+++ src/app/modules/OfferedCourse/offeredCourse.service.ts
@@ -2,46 +2,58 @@ import { TOfferedCourse } from './offeredCourse.interface';
 import { OfferedCourse } from './offeredCourse.model';
 import { SemesterRegistration } from './../semesterRegistration/semisterRegistration.model';
 import { AppError } from '../../Errors/AppErrors';
-import  httpStatus  from 'http-status';
+import httpStatus from 'http-status';
 import { AcademicFaculty } from '../academicFaculty/academicFaculty.model';
 import { AcademicDepartment } from '../academicDepartment/academicDepartment.model';
 import { Course } from '../course/course.model';
 import { Faculty } from '../Faculty/faculty.model';
 
 const createOfferedCourseIntoDb = async (payload: TOfferedCourse) => {
-  const {semesterRegistration ,academicFaculty, academicDepartment, course , faculty } = payload; 
-  //! Check if the semester registration id is exists
-  const isSemesterRegistrationExits = await SemesterRegistration.findById(semesterRegistration)
+  const {
+    semesterRegistration,
+    academicFaculty,
+    academicDepartment,
+    course,
+    faculty,
+  } = payload;
+  //! Check if the SemesterRegistration id is exists
+  const isSemesterRegistrationExits =
+    await SemesterRegistration.findById(semesterRegistration);
 
-  if(!isSemesterRegistrationExits){
-    throw new AppError(httpStatus.NOT_FOUND, "Semester Registration not found")
+  if (!isSemesterRegistrationExits) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Semester Registration not found');
   }
-  //! Check if the semester registration id is exists
-  const isAcademicFacultyExits = await AcademicFaculty.findById(academicFaculty)
+  //? Extracting academicSemester from isSemesterRegistrationExits
+  const academicSemester = isSemesterRegistrationExits.academicSemester
 
-  if(!isAcademicFacultyExits){
-    throw new AppError(httpStatus.NOT_FOUND, "Academic Faculty not found")
+  //! Check if the AcademicFaculty id is exists
+  const isAcademicFacultyExits =
+    await AcademicFaculty.findById(academicFaculty);
+
+  if (!isAcademicFacultyExits) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Academic Faculty not found');
   }
-  //! Check if the semester registration id is exists
-  const isAcademicDepartmentExits = await AcademicDepartment.findById(academicDepartment)
+  //! Check if the AcademicDepartment id is exists
+  const isAcademicDepartmentExits =
+    await AcademicDepartment.findById(academicDepartment);
 
-  if(!isAcademicDepartmentExits){
-    throw new AppError(httpStatus.NOT_FOUND, "Academic Department not found")
+  if (!isAcademicDepartmentExits) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Academic Department not found');
   }
-  //! Check if the semester registration id is exists
-  const isCourseExits = await Course.findById(course)
+  //! Check if the Course id is exists
+  const isCourseExits = await Course.findById(course);
 
-  if(!isCourseExits){
-    throw new AppError(httpStatus.NOT_FOUND, "Course not found")
+  if (!isCourseExits) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Course not found');
   }
-  //! Check if the semester registration id is exists
-  const isFacultyExits = await Faculty.findById(faculty)
+  //! Check if the Faculty id is exists
+  const isFacultyExits = await Faculty.findById(faculty);
 
-  if(!isFacultyExits){
-    throw new AppError(httpStatus.NOT_FOUND, "Faculty not found")
+  if (!isFacultyExits) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Faculty not found');
   }
 
-  const result = await OfferedCourse.create(payload);
+  const result = await OfferedCourse.create({...payload, academicSemester});
   return result;
 };
 
