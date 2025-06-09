615c42e88ec7b4b1b883d013556a8a5b2b66bbde
mohammadrivy <rizvyhq1@gmail.com>
2025-06-10 04:12:15 +0600

    fix(OfferedCourse): define TSchedules type for schedule management and update import statements

--- src/app/modules/OfferedCourse/OfferCourse.utils.ts
+++ src/app/modules/OfferedCourse/OfferCourse.utils.ts
@@ -1,13 +1,11 @@
-import { TDays } from "./offeredCourse.interface"
+import { TSchedules } from "./offeredCourse.interface";
 
-export type TSchedules = {
-    days: TDays,
-    startTime: string;
-    endTime: string;
-}
+
 
 
 const hasTimeConflict = (assignedSchedules : TSchedules[], schedules : TSchedules) => {
+
+    
 
 
 }
--- src/app/modules/OfferedCourse/offeredCourse.interface.ts
+++ src/app/modules/OfferedCourse/offeredCourse.interface.ts
@@ -14,3 +14,9 @@ export type TOfferedCourse = {
   startTime: string;
   endTime: string;
 };
+
+export type TSchedules = {
+    days: TDays,
+    startTime: string;
+    endTime: string;
+}
