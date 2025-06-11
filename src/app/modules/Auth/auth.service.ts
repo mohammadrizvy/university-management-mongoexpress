0296f9c5384ce85c4a0c2a4ab0cd60993d78178e
mohammadrivy <rizvyhq1@gmail.com>
2025-06-11 23:02:58 +0600

    refactor(Auth): improve loginUser function and change TUser type to interface

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -2,6 +2,7 @@ import { AppError } from '../../Errors/AppErrors';
 import { User } from '../user/user.model';
 import { TLoginUser } from './auth.interface';
 import httpStatus from 'http-status';
+import bcrypt from "bcrypt"
 
 const loginUser = async (payload: TLoginUser) => {
   // Checking if the user dose exists ?
@@ -16,14 +17,25 @@ const loginUser = async (payload: TLoginUser) => {
 
   const isDeleted = isUserExists?.isDeleted;
 
-  if(isDeleted){
-    throw new AppError(httpStatus.FORBIDDEN, 'The user is deleted');   
+  if (isDeleted) {
+    throw new AppError(httpStatus.FORBIDDEN, 'The user is deleted');
   }
   const userStatus = isUserExists?.status;
 
-  if(userStatus === "blocked"){
-    throw new AppError(httpStatus.FORBIDDEN, 'The user is blocked');   
+  if (userStatus === 'blocked') {
+    throw new AppError(httpStatus.FORBIDDEN, 'The user is blocked');
   }
+
+
+  // Checking if the password is correct ? 
+
+  const isPasswordMatch =  await bcrypt.compare(payload.password , isUserExists.password)
+
+  console.log(isPasswordMatch)
+
+
+  // Access Granted : Send AccessToken , RefreshToken 
+
 
   return {};
 };
--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,4 +1,4 @@
-export type TUser = {
+export interface TUser  {
   id: string;
   password: string;
   needsPasswordChange: boolean;
@@ -6,9 +6,3 @@ export type TUser = {
   status: 'in-progress' | 'blocked';
   isDeleted: boolean;
 };
-
-// export type NewUser = {
-//   password: string;
-//   role: string;
-//   id : string
-// };
