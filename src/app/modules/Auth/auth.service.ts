e9dcd0c743d46856b7ee4c40aa5770551af4683e
mohammadrivy <rizvyhq1@gmail.com>
2025-06-11 22:23:19 +0600

    feat(Auth): enhance loginUser function with user deletion and blocking checks

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -9,9 +9,20 @@ const loginUser = async (payload: TLoginUser) => {
   const isUserExists = await User.findOne({ id: payload?.id });
 
   console.log(isUserExists);
-  
+
   if (!isUserExists) {
     throw new AppError(httpStatus.NOT_FOUND, 'User not found');
+  }
+
+  const isDeleted = isUserExists?.isDeleted;
+
+  if(isDeleted){
+    throw new AppError(httpStatus.FORBIDDEN, 'The user is deleted');   
+  }
+  const userStatus = isUserExists?.status;
+
+  if(userStatus === "blocked"){
+    throw new AppError(httpStatus.FORBIDDEN, 'The user is blocked');   
   }
 
   return {};
