9e8b73c19b53aae92a8554fca1b53ff64c4e09a3
mohammadrivy <rizvyhq1@gmail.com>
2025-06-13 00:08:22 +0600

    fix(env): correct JWT_ACCESS_SECRET value in .env file and update auth middleware for better error handling

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,23 +1,37 @@
 import { NextFunction, Request, Response } from 'express';
 import catchAsync from '../utils/catchAsync';
 import { AppError } from '../Errors/AppErrors';
-import  httpStatus  from 'http-status';
+import httpStatus from 'http-status';
+import jwt from 'jsonwebtoken';
+import config from '../config';
 
-// !Express middleware to checking authorization !!! 
+// !Express middleware to checking authorization !!!
 const auth = () => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-  
-    const token = (req.headers.authorization)
-     console.log(token)
+    const token = req.headers.authorization;
+    // ?Checking if the token is sended from the the client or no !
+    if (!token) {
+      throw new AppError(httpStatus.UNAUTHORIZED, "You're not authorized user");
+    }
+    // ?Checking if the token is valid or not !
+    // invalid token
+    jwt.verify(
+      token,
+      config.jwt_access_secret as string,
+      function (err, decoded) {
+        // err
+        if (err) {
+          throw new AppError(
+            httpStatus.UNAUTHORIZED,
+            "You're not authorized user (INVALID TOKEN) ",
+          );
+        }
+        // decoded undefined
+        console.log(decoded);
+      },
+    );
 
-     if(!token){
-      throw new AppError(
-          httpStatus.UNAUTHORIZED,
-          "You're not authorized user ",
-        );
-     }
-    
-    next(); 
+    next();
   });
 };
 
--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -38,11 +38,11 @@ const loginUser = async (payload: TLoginUser) => {
 
   // Create token and sent to the client
   const jwtPayload = {
-    userId: user,
+    userId: user.id,
     role: user.role,
   };
   const accessToken = jwt.sign(jwtPayload, config.jwt_access_secret as string, {
-    expiresIn: '30',
+    expiresIn: '7d',
   });
 
   return {
