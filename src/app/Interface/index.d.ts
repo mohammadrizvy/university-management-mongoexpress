00842fa696ae438e2629ea3b79f53e11f4f8de3c
mohammadrivy <rizvyhq1@gmail.com>
2025-06-13 00:38:51 +0600

    feat(auth): enhance authorization middleware to include user payload in request object

---
+++ src/app/Interface/index.d.ts
@@ -0,0 +1,10 @@
+import { JwtPayload } from 'jsonwebtoken';
+
+declare global {
+    import { Request } from 'express';
+    namespace Express {
+        interface Request {
+            user: JwtPayload
+        }
+    }
+}
--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -2,37 +2,45 @@ import { NextFunction, Request, Response } from 'express';
 import catchAsync from '../utils/catchAsync';
 import { AppError } from '../Errors/AppErrors';
 import httpStatus from 'http-status';
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
+
+
 
 // !Express middleware to checking authorization !!!
 const auth = () => {
-  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization;
-    // ?Checking if the token is sended from the the client or no !
-    if (!token) {
-      throw new AppError(httpStatus.UNAUTHORIZED, "You're not authorized user");
-    }
-    // ?Checking if the token is valid or not !
-    // invalid token
-    jwt.verify(
-      token,
-      config.jwt_access_secret as string,
-      function (err, decoded) {
-        // err
-        if (err) {
-          throw new AppError(
-            httpStatus.UNAUTHORIZED,
-            "You're not authorized user (INVALID TOKEN) ",
-          );
-        }
-        // decoded undefined
-        console.log(decoded);
-      },
-    );
+  return catchAsync(
+    async (req: Request, res: Response, next: NextFunction) => {
+      const token = req.headers.authorization;
+      // ?Checking if the token is sended from the the client or no !
+      if (!token) {
+        throw new AppError(
+          httpStatus.UNAUTHORIZED,
+          "You're not authorized user",
+        );
+      }
+      // ?Checking if the token is valid or not !
+      // invalid token
+      jwt.verify(
+        token,
+        config.jwt_access_secret as string,
+        function (err, decoded) {
+          // err
+          if (err) {
+            throw new AppError(
+              httpStatus.UNAUTHORIZED,
+              "You're not authorized user (INVALID TOKEN) ",
+            );
+          }
+          // decoded
 
-    next();
-  });
+          req.user = decoded as JwtPayload;
+        },
+      );
+
+      next();
+    },
+  );
 };
 
 export default auth;
