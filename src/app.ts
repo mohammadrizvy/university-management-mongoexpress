cf5139421e3b21fd2b936ee0b0f181c825ef4164
mohammadrivy <rizvyhq1@gmail.com>
2025-06-14 03:11:15 +0600

    feat(auth): add cookie-parser middleware and update package dependencies

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import cors from 'cors';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFoundRoute from './app/middlewares/notFoundRoute';
 import router from './app/Routes';
+import cookieParser from 'cookie-parser'
 const app: Application = express();
 
 // parsers
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser())
 
 //applications routes
 app.use('/api/v1', router);
