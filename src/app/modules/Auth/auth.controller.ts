bab4bae33b245980bb5eb0e518fcd51bae146940
mohammadrivy <rizvyhq1@gmail.com>
2025-06-14 02:51:21 +0600

    fix(auth): correct NODE_ENV value and update loginUser response structure

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -2,15 +2,23 @@ import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
 import httpStatus from 'http-status';
 import { authServices } from './auth.service';
+import config from '../../config';
 
 const loginUser = catchAsync(async (req, res, next) => {
   const result = await authServices.loginUser(req.body);
+  const {refreshToken , accessToken , needsPasswordChange } = result; 
+  res.cookie("refreshToken" , refreshToken, {
+    secure : config.NODE_ENV === "production",
+    httpOnly : true
+  })
 
   sendResponse(res, {
     sucess: true,
     statusCode: httpStatus.FOUND,
     message: 'User logged in sucessfully',
-    data: result,
+    data: {
+      accessToken , needsPasswordChange
+    },
   });
 });
 
